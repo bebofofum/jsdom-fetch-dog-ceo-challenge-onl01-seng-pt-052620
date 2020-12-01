e4a733f953d1f062b45a734c3ae85f96cdb68e5b
bebofofum <bri.boehm@gmail.com>
2020-11-30 21:18:36 -0500

    commit tree ed9374f4adea5a88bfac765cc6bf2acdece8a720

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+function fetchImages() {
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+  fetch(imgUrl)
+  .then(function(response) {
+    return response.json();
+ })
+ .then(function(json) {
+   renderImages(json)
+ });
+
+}
+
+function renderImages(images) {
+  const container = document.querySelector('dog-image-container')
+
+  images.forEach(image => {
+    const link = document.createElement('a')
+    const elem = document.createElement('img')
+    elem.src = image
+    container.appendChild(elem)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImages()
+})
